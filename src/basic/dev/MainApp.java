cb261ab82de721c267264c1eec04bce395bea7ec
TrungNguyen0304 <113655575+TrungNguyen0304@users.noreply.github.com>
2022-10-19 20:18:51 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,26 @@
+package basic.dev;
+/**
+ * baitap5
+ * @author NguyenDinhTrung
+ * Class 22cdth41
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		/**
+		 * 
+		 */
+		int number;
+		String nameSchool;
+
+
+	}
+/**
+ * 
+ */
+	public void show() {
+}
+}
